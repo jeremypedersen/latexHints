d201fb9f732d3f7738a598605278bfa749f3d797
JDP <jeremydavispedersen@gmail.com>
2023-05-15 10:12:46 +0000

    Added additional scripts to install texlive and build the example PDF

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build the example PDF on Linux systems (on mac, use TexLive)
+pdflatex latex_hints.tex
---
+++ install-tex.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install full TexLive, needed to build the example PDF
+# on Linux systems
+sudo apt isntall texlive-full
